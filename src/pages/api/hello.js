3a6d3cc175b52832d22f29d6f56afd23a10dd23a
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-12-29 17:53:48 +0700

    create example for api

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import Payload from "@/Payload/Payload"
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+  const payload = new Payload(200, { name: 'John Doe' })
+
+  res.status(payload.status).json(payload.data)
 }
